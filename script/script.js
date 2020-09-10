12bdcaf2d7941cc6aa523034a160fc3bba05db87
Limon1980 <tarkalmob@yandex.ru>
2020-09-10 23:03:55 +1000

    реализовал по видео урока отправка данных на сервер

--- script/script.js
+++ script/script.js
@@ -60,7 +60,7 @@ window.addEventListener('DOMContentLoaded', () => {
     body.addEventListener('click', (event) => {
       let target = event.target;
 
-      if (target.parentNode === btnMenu ) {
+      if (target.parentNode === btnMenu) {
         handlerMenu();
       } else {
         target = target.closest('.active-menu');
@@ -278,127 +278,170 @@ window.addEventListener('DOMContentLoaded', () => {
   // data атрибуты
 
   const team = () => {
-  const command = document.querySelectorAll('.command__photo');
+    const command = document.querySelectorAll('.command__photo');
 
-  command.forEach((elem) => {
-    elem.addEventListener('mouseenter', (event) => {
-    event.target.src = event.target.dataset.img;
+    command.forEach((elem) => {
+      elem.addEventListener('mouseenter', (event) => {
+        event.target.src = event.target.dataset.img;
+      });
+
+      elem.addEventListener('mouseleave', (event) => {
+        event.target.src = event.target.dataset.img.replace(/a(?=.jpg)/, '');
+      });
     });
+  };
 
-    elem.addEventListener('mouseleave', (event) => {
+  team();
 
-      event.target.src = event.target.dataset.img.replace(/a(?=.jpg)/, '');
-      
+  // валидация калькулятора
+  const calcValidate = () => {
+    const calcBlock = document.querySelector('.calc-block');
+
+    calcBlock.addEventListener('input', (e) => {
+      if (
+        e.target.matches('.calc-square') ||
+        e.target.matches('.calc-count') ||
+        e.target.matches('.calc-day')
+      ) {
+        e.target.value = e.target.value.replace(/\D/g, '');
+      }
     });
+  };
+  calcValidate();
 
-  });
-};
+  // калькулятор
 
-team();
-
- // валидация калькулятора
- const calcValidate = () => {
-  const calcBlock = document.querySelector('.calc-block');
-
-  calcBlock.addEventListener('input', (e) => {
-    if (e.target.matches('.calc-square') ||
-      e.target.matches('.calc-count') ||
-      e.target.matches('.calc-day')) {
-      e.target.value = e.target.value.replace(/\D/g, '');
-    }
-  });
-};
-calcValidate();
-
-// калькулятор
-
-const calc = (price = 100) => {
-  const calcBlock = document.querySelector('.calc-block'),
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
       calcType = document.querySelector('.calc-type'),
       calcSquare = document.querySelector('.calc-square'),
       calcDay = document.querySelector('.calc-day'),
       calcCount = document.querySelector('.calc-count'),
       totalValue = document.getElementById('total');
 
-    
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
 
-      const countSum = () => {
-        let total = 0,
-            countValue = 1,
-            dayValue = 1;
-        const typeValue = calcType.options[calcType.selectedIndex].value,
-               squareValue = +calcSquare.value;
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
 
-              if(calcCount.value > 1){
-                countValue += (calcCount.value - 1) / 10;
-              }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
 
-              if (calcDay.value && calcDay.value < 5) {
-                dayValue *= 2;
-              } else if (calcDay.value && calcDay.value < 10) {
-                dayValue *= 1.5;
-              }
-               
-        if(typeValue && squareValue){
-          total = Math.ceil(price * typeValue * squareValue * countValue * dayValue);
-        }
+      if (typeValue && squareValue) {
+        total = Math.ceil(
+          price * typeValue * squareValue * countValue * dayValue
+        );
+      }
 
-        // let iter = 0;
-        // const cycleDigit = () =>{
-  
-        //   totalValue.innerText = iter;
-        //   if (iter < total){
-        //     setTimeout(cycleDigit, 10);
-        //     iter = iter + Math.ceil(total/100);
-        //   }
-        // };
+      // let iter = 0;
+      // const cycleDigit = () =>{
 
-        function cycleDigit() {
-          let counter = 0;
-            return function count() {
-              
-               if (counter >= total){
-                totalValue.innerText = total;
-              } else if(counter % 100 === 0){
-                totalValue.innerText = counter;
-                setTimeout(count, 10);
-              }
-                counter = counter + 100;
-               return counter;
-            };
-        }
-        
-        let cicle = cycleDigit();
-    
+      //   totalValue.innerText = iter;
+      //   if (iter < total){
+      //     setTimeout(cycleDigit, 10);
+      //     iter = iter + Math.ceil(total/100);
+      //   }
+      // };
 
-        if (total > 0){
-          cicle();
-          console.log(total);
-        } else {
-          totalValue.textContent = 0;
-        }
-        
-      };
-
-     
-
-      calcBlock.addEventListener('change', (event) => {
-        const target = event.target;
-        // if(target.matches('.calc-type') || target.matches('.calc-squre') || 
-        // target.matches('.calc-day') || target.matches('.calc-count')){
-          
-        // }
-          // if (target === calcType || target === calcSquare ||
-          //   target === calcDay || target === calcCount){
-          //     console.log();
-          //   }
-
-          if (target.matches('select') || target.matches('input')){
-            countSum();
+      function cycleDigit() {
+        let counter = 0;
+        return function count() {
+          if (counter >= total) {
+            totalValue.innerText = total;
+          } else if (counter % 100 === 0) {
+            totalValue.innerText = counter;
+            setTimeout(count, 10);
           }
+          counter = counter + 100;
+          return counter;
+        };
+      }
+
+      let cicle = cycleDigit();
+
+      if (total > 0) {
+        cicle();
+        console.log(total);
+      } else {
+        totalValue.textContent = 0;
+      }
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      // if(target.matches('.calc-type') || target.matches('.calc-squre') ||
+      // target.matches('.calc-day') || target.matches('.calc-count')){
+
+      // }
+      // if (target === calcType || target === calcSquare ||
+      //   target === calcDay || target === calcCount){
+      //     console.log();
+      //   }
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
+  };
+
+  calc(100);
+
+  // send-ajax-form
+
+  const sendForm = () => {
+    const errorMessage = 'Что то пошло не так',
+      loadMessage = 'Загрузка',
+      successMessage = 'Спасибо! Мы скоро с вами свяжемся';
+
+    const form = document.getElementById('form1');
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2rem;';
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      form.appendChild(statusMessage);
+
+      const request = new XMLHttpRequest();
+
+      request.addEventListener('readystatechange', () => {
+        statusMessage.textContent = loadMessage;
+
+        if (request.readyState !== 4) {
+          return;
+        }
+
+        if (request.status === 200) {
+          statusMessage.textContent = successMessage;
+        } else {
+          statusMessage.textContent = errorMessage;
+        }
       });
-};
 
-calc(100);
+      request.open('POST', './server.php');
+      request.setRequestHeader('Content-Type', 'aplication/json');
+      const formData = new FormData(form);
+      let body = {};
+
+      // for (let val of formData.entries()) {
+      //   body[val[0]] = val[1];
+      // }
+
+      formData.forEach((val, key) => {
+        body[key] = val;
+      });
+      console.log(body);
+      request.send(JSON.stringify(body));
+    });
+  };
+
+  sendForm();
 });
-
